d35142ef447faddbf387cb772a0263aa8dd1706e
Jon-Arve Constantine Grønsberg-Ovesen <jonovesen@gmail.com>
2026-08-25 01:02:30 +0200

    Add app entry point

---
+++ CyberKey/CyberKeyApp.swift
@@ -0,0 +1,20 @@
+//
+//  CyberKeyApp.swift
+//  CyberKey
+//
+//  Created by Cybergroup Incorporated
+//  Copyright © 2026 Cybergroup Incorporated. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CyberKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark)
+                .statusBarHidden(true)
+        }
+    }
+}
